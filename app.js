042b229c3b6560ff58cac7e8c22ff984a33cd39c
Minki Baek <alsrl6678@naver.com>
2024-05-09 17:51:46 +0900

    새로운 fly계정생성

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ import restCtrl from "./app/src/restaurants/restaurants.ctrl.js";
 import reviewCtrl from "./app/src/Reviews/review.ctrl.js";
 const { Pool } = pkg;
 /* 
-Postgres cluster makterback2db created
+Postgres cluster makterback created
   Username:    postgres
-  Password:    vHurqTSDOMLs5jE
-  Hostname:    makterbackdf.internal
-  Flycast:     fdaa:5:35c8:0:1::16
+  Password:    ldP6Kqw4w7nhIPM
+  Hostname:    makterback.internal
+  Flycast:     fdaa:5:35c8:0:1::1d
   Proxy port:  5432
   Postgres port:  5433
-  Connection string: postgres://postgres:1nX7mGNmv0HsbSZ@makterback2db.flycast:5432
-*/
+  Connection string: postgres://postgres:ldP6Kqw4w7nhIPM@makterback.flycast:5432
+  */
 const pool = new Pool({
   user: "postgres",
-  password: "vHurqTSDOMLs5jE",
+  password: "ldP6Kqw4w7nhIPM",
   host: "127.0.0.1",
   database: "postgres",
   port: 5432,
@@ -33,7 +33,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "token"], // "token" 헤더를 허용합니다.
@@ -55,8 +55,11 @@ app.put("/api/v1/reviews/:review_id", reviewCtrl.remotereview);
 // 예시: 리뷰 삭제
 app.delete("/api/v1/reviews/:review_id", reviewCtrl.deletereview);
 
+// 모든 리뷰 조회
+app.get("/api/v1/reviews/:review_id", reviewCtrl.getAllReviews);
+
 // 예시: 특정 식당의 리뷰 목록 조회
-app.get("/api/v1/restaurants/:restaurant_id/reviews", reviewCtrl.restreview);
+app.get("/api/v1/restaurants/reviews", reviewCtrl.restreview);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
--- app/src/Reviews/review.ctrl.js
+++ app/src/Reviews/review.ctrl.js
@@ -3,14 +3,16 @@ import { pool } from "../../../app.js";
 
 const createreview = async (req, res) => {
   try {
-    const { restaurants_id, review_text, user_id } = req.body;
+    const { restaurant_id, contents, username, rating } = req.body;
+    const date = new Date().toISOString().slice(0, 10); // 현재 날짜를 ISO 형식으로 가져와서 YYYY-MM-DD 형식으로 변환
+
     const { rows } = await pool.query(
       `
-        INSERT INTO reviews (restaurants_id, review_text, review_date, user_id) 
-        VALUES ($1, $2, CURRENT_TIMESTAMP, $3)
+        INSERT INTO reviews (restaurant_id, contents, date, rating, username) 
+        VALUES ($1, $2, $3, $4, $5)
         RETURNING *
         `,
-      [restaurants_id, review_text, user_id]
+      [restaurant_id, contents, date, rating, username]
     );
     res.json({
       resultCode: "S-1",
@@ -29,15 +31,15 @@ const createreview = async (req, res) => {
 const remotereview = async (req, res) => {
   try {
     const { id } = req.params;
-    const { restaurant_id, review_text, user_id } = req.body;
+    const { restaurant_id, contents, username, rating } = req.body;
     const { rows } = await pool.query(
       `
         UPDATE reviews 
-        SET restaurant_id = $1, review_text = $2, review_date = CURRENT_TIMESTAMP, user_id = $4
-        WHERE id = $5
+        SET restaurant_id = $1, contents = $2, review_date = CURRENT_TIMESTAMP, username = $4 , rating = $5 
+        WHERE id = $6
         RETURNING *
         `,
-      [restaurant_id, review_text, user_id, id]
+      [restaurant_id, contents, username, rating]
     );
     res.json({
       resultCode: "S-1",
@@ -82,6 +84,46 @@ const deletereview = async (req, res) => {
   }
 };
 
+// 모든 리뷰조회
+const getAllReviews = async (req, res) => {
+  try {
+    const { restaurant_id } = req.params;
+    const reviews = await pool.query(
+      `
+      SELECT 
+          r.id AS review_id,
+          r.username,
+          r.contents AS review_contents,
+          r.date AS review_date,
+          r.rating,
+          r.restaurant_id,
+          array_agg(h.contents) AS hashtags
+      FROM 
+          reviews AS r
+      LEFT JOIN
+          reviews_hashtags AS rh ON r.id = rh.reviews_id
+      LEFT JOIN
+          hashtags AS h ON rh.hashtags_id = h.id
+      GROUP BY
+          r.id
+    `,
+      [restaurant_id]
+    );
+    res.json({
+      resultCode: "S-1",
+      msg: "Success",
+      data: reviews.rows,
+    });
+  } catch (error) {
+    console.error("Error fetching reviews:", error);
+    res.status(500).json({
+      resultCode: "F-1",
+      msg: "Error fetching reviews",
+      error: error.message,
+    });
+  }
+};
+
 //사용자 리뷰
 const userreview = async (req, res) => {
   try {
@@ -107,21 +149,26 @@ const userreview = async (req, res) => {
 //식당 리뷰
 const restreview = async (req, res) => {
   try {
-    const { restaurant_id } = req.params; // 여기서 restaurants_id가 아닌 restaurant_id로 수정
-    const { rows } = await pool.query(
-      "SELECT * FROM reviews WHERE restaurant_id = $1",
-      [restaurant_id]
+    // const { restaurant_id } = req.params;
+    const reviews = await pool.query(
+      `SELECT r.*, array_agg(h.contents) AS hashtags
+       FROM reviews AS r
+       LEFT JOIN reviews_hashtags AS rh ON r.id = rh.reviews_id
+       LEFT JOIN hashtags AS h ON rh.hashtags_id = h.id
+       GROUP BY r.id`,
+      [] // restaurant_id 매개변수 전달
     );
     res.json({
       resultCode: "S-1",
-      msg: "성공",
-      data: rows,
+      msg: "Success",
+      data: reviews.rows,
     });
   } catch (error) {
-    console.error(error);
+    console.error("Error fetching reviews:", error);
     res.status(500).json({
       resultCode: "F-1",
-      msg: "에러 발생",
+      msg: "Error fetching reviews",
+      error: error.message,
     });
   }
 };
@@ -130,6 +177,7 @@ export default {
   createreview,
   remotereview,
   deletereview,
+  getAllReviews,
   userreview,
   restreview,
 };
